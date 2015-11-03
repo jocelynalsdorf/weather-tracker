806eee71ad32bf38a27242c6915cc2379d56886f
jocelynalsdorf <jfrodsla@gmail.com>
2015-11-03 12:16:58 -0800

    add json api call; is retrieving correct data, needs formatting and use of lat/long vars

--- app.js
+++ app.js
@@ -9,21 +9,30 @@ $(document).ready(function() {
   //get location of user lat/long  
   window.onload = function() {
     var startPos;
+    //speed up call by allowing for cached results
+    var geoOptions = {
+    maximumAge: 5 * 60 * 1000,
+  }
     var geoSuccess = function(position) {
       startPos = position;
       document.getElementById('startLat').innerHTML = startPos.coords.latitude;
       document.getElementById('startLon').innerHTML = startPos.coords.longitude;
     };
     //handle errors with lookup
-    // var geoError = function(error) {
-    //   console.log('Error occurred. Error code: ' + error.code);
-    //   // error.code can be:
-    //   //   0: unknown error
-    //   //   1: permission denied
-    //   //   2: position unavailable (error response from location provider)
-    //   //   3: timed out
-    // };
+    var geoError = function(error) {
+      console.log('Error occurred. Error code: ' + error.code);
+      // error.code can be:
+      //   0: unknown error
+      //   1: permission denied
+      //   2: position unavailable (error response from location provider)
+      //   3: timed out
+    };
     navigator.geolocation.getCurrentPosition(geoSuccess);
   };
+
+
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=45&lon=122&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f", function(json) {
+        $(".message").html(JSON.stringify(json));
+      });
 
 });
